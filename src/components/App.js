3bb79cdb7c78b07545ac3405982acee5c4d43f1c
WonilMon <ll350ll@naver.com>
2025-05-11 16:36:50 +0900

    250511 Update_log

--- src/components/App.js
+++ src/components/App.js
@@ -1,107 +1,10 @@
-// import React, { Component } from 'react';
-// import Navbar from './Navbar';
-// import Web3 from 'web3';
-// import Tether from '../truffle_abis/Tether.json';
-// import RWD from '../truffle_abis/RWD.json';
-// import DecentralBank from '../truffle_abis/DecentralBank.json';
-// import './App.css';
-
-// class App extends Component {
-
-//     // mount before rendering
-//     async UNSAFE_componentWillMount () {
-//         await this.loadWeb3()
-//         await this.loadBlockchainData()
-//     }
-
-//     async loadWeb3() {
-//         if(window.ethereum) {
-//             window.web3 = new Web3(window3.ethereum)
-//             await window.ethereum.enable()
-//         } else if (window.web3) {
-//             window.web3 = new Web3(window.web3.currentProvider)
-//         } else {
-//             window.alert('No ethereum browser detected! You can check out MetaMask!')
-//         }
-//     }
-
-//     async loadBlockchainData () {
-//         const web3 = new window.web3
-//         const account = await web3.eth.getAccount()
-//         this.setState({account : account[0]} )
-
-//         // Load network ID
-//         const networkId = await web3.eth.net.getId()
-//         // Load Tether Contract
-//         const tetherData = Tether.networks[networkId]
-//         if(tetherData) {
-//             const tether = new web3.eth.Contract(Tether.abi, tetherData.address)
-//             this.setState({tether})
-//             let tetherBalance = await tether.methods.balanceOf(this.state.account).call()
-//             this.setState({tetherBalance : tetherBalance.toString()})
-//         } else {
-//             window.alert('Error! Tether contract not deployed - no detected network!')
-//         }
-//         // Load RWD Contract
-//         const rwdData = RWD.networks[networkId]
-//         if (rwdData) {
-//             const rwd = new web3.eth.Contract(RWD.abi, rwdData.address)
-//             this.setState({rwd})
-//             let rwdBalance = await rwd.methods.balanceOf(this.state.account).call()
-//             this.setState({rwdBalance : rwdBalance.toString()})
-//         } else {
-//             window.alert('Error! RWD contract not deployed - no detected network!')
-//         }
-//         // Load DecentralBank Contract
-//         const decentralBankData = DecentralBank.networks[networkId]
-//         if (decentralBankData) {
-//             const decentralBank = new web3.eth.Contract(DecentralBank.abi, decentralBankData.address)
-//             this.setState({decentralBank})
-//             let stakingBalance = await decentralBank.methods.stakingBalance(this.state.account).call()
-//             this.setState({stakingBalance : stakingBalance.toString()})
-//         } else {
-//             window.alert('Error! DecentralBank contract not deployed - no detected network!')
-//         }
-
-//         this.setState({loading : false} )
-//     }
-
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       account: '0x0',
-//       tether : {},
-//       rwd : {},
-//       decentralBank : {},
-//       tetherBalance : '0',
-//       rwdBalance : '0',
-//       stakingBalance : '0',
-//       loading : true
-//     };
-//   }
-
-//   //Our React code Goes In Here!
-//   render() {
-//     return (
-//       <div>
-//         <Navbar account={this.state.account}>
-//           <div className="text-center">
-//             <h></h>
-//           </div>
-//         </Navbar>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
 import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
 import Navbar from './Navbar';
 import Tether from '../truffle_abis/Tether.json';
 import RWD from '../truffle_abis/RWD.json';
 import DecentralBank from '../truffle_abis/DecentralBank.json';
+import Main from './Main.js';
 import './App.css';
 
 const App = () => {
@@ -199,17 +102,14 @@ const App = () => {
   return (
     <div>
       <Navbar account={account} />
-      <div className="text-center">
-        {loading ? (
-          <h1>Loading...</h1>
-        ) : (
-          <>
-            <h1>Welcome to Decentralized Bank</h1>
-            <p>Tether Balance: {tetherBalance}</p>
-            <p>RWD Balance: {rwdBalance}</p>
-            <p>Staking Balance: {stakingBalance}</p>
-          </>
-        )}
+      <div className='container-fluid mt-5'>
+        <div className='row'>
+            <main role='main' className='col-lg-12 ml-auto mr-auto' style={{maxWidth:'600px', minHeight:'100vm'}} >
+                <div>
+                    <Main/>
+                </div>
+            </main>
+        </div>
       </div>
     </div>
   );
