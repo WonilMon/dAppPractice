cb3bc1a092e7d2631f6e8b6b4969397686834c3d
WonilMon <ll350ll@naver.com>
2025-05-19 22:04:30 +0900

    250519 Update_migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,8 @@
 const Tether = artifacts.require('Tether');
 const RWD = artifacts.require('RWD');
 const DecentralBank = artifacts.require('DecentralBank');
-// const Ballot = artifacts.require('Ballot');
 
-module.exports = async function(deployer, network, accounts) {
+module.exports = async function (deployer, network, accounts) {
   // Deploy Mock Tether Contract
   await deployer.deploy(Tether);
   const tether = await Tether.deployed();
@@ -15,10 +14,6 @@ module.exports = async function(deployer, network, accounts) {
   // Deploy Mock Tether DecentralBank
   await deployer.deploy(DecentralBank, rwd.address, tether.address);
   const decentralBank = await DecentralBank.deployed();
-
-  // // Deploy Ballot
-  // await deployer.deploy(RWD);
-  // const rwd = await RWD.deployed();
 
   // Transfer all RWD token to Decentral Bank
   await rwd.transfer(decentralBank.address, '1000000000000000000000000');
